465c7993c09235deaf79d1815c38eaceb417f244
ZRaykh <zraykh2784@bths.edu>
2022-09-27 14:33:51 -0400

    Finished assignment

--- src/U2L2Runner.java
+++ src/U2L2Runner.java
@@ -1,24 +1,56 @@
+import java.util.Scanner;
 public class U2L2Runner {
     public static void main(String[] args) {
 
-        Rectangle r1 = new Rectangle(150, 200); //Rectangle creation
-        Rectangle r2 = new Rectangle(100);
-        Rectangle r3 = new Rectangle();
+        Poem poem = new Poem("Adrian");
+        poem.printPoem();
 
-        r1.setWidth(125); //Changing width ex 2
-        r2.setWidth(125);
-        r2.setLength(125);
-        r3.setWidth(125);
+        //Scanner s = new Scanner(System.in);
 
-        r1.printRectangleInfo(); //printing rectangle data
-        r2.printRectangleInfo();
-        r3.printRectangleInfo();
+        //Getting inputs for boards of lumber, windows, and tax
+        //System.out.print("How many boards of lumber do you need?");
+        //int lumber = s.nextInt();
 
-        System.out.print("The total length of fencing needed (all three rectangles) is:"); //prints the perimeter of all three Rectangles.
-        System.out.print(r1.calculatePerimeter() + r2.calculatePerimeter() + r3.calculatePerimeter());
-        System.out.println("ft.");
-        System.out.print("The total grass seed needed is:");
-        System.out.print(r1.calculateArea() + r2.calculateArea() + r3.calculateArea());
-        System.out.print("sq ft.");
+        //System.out.print("How many windows do you need?");
+        // int windows = s.nextInt();
+
+        //System.out.print("What is the tax rate for your area?");
+        // double taxRate = s.nextDouble();
+
+        //Creating new constructor
+        // ConstructionPricer cost = new ConstructionPricer(11.50, 25.75, taxRate);
+
+        //Calculating material cost
+        // double materials = cost.materialsCost(lumber, windows);
+
+        //Calculating total with tax
+        //double total = cost.totalWithTax(materials);
+
+        //printing values out
+        // System.out.println("Total materials cost: " + materials);
+        //System.out.print("Grand total after tax: " + total);
+
+
+
+        // Rectangle r1 = new Rectangle(150, 200); //Rectangle creation
+        //Rectangle r2 = new Rectangle(100);
+        // Rectangle r3 = new Rectangle();
+
+        //r1.setWidth(125); //Changing width ex 2
+        //r2.setWidth(125);
+        //r2.setLength(125);
+        //r3.setWidth(125);
+
+        //r1.printRectangleInfo(); //printing rectangle data
+        //r2.printRectangleInfo();
+        //r3.printRectangleInfo();
+
+        //System.out.print("The total length of fencing needed (all three rectangles) is:"); //prints the perimeter of all three Rectangles.
+        //System.out.print(r1.calculatePerimeter() + r2.calculatePerimeter() + r3.calculatePerimeter());
+        //System.out.println("ft.");
+        //System.out.print("The total grass seed needed is:");
+        //System.out.print(r1.calculateArea() + r2.calculateArea() + r3.calculateArea());
+        //System.out.print("sq ft.");
+
     }
 }
